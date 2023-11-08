22db9115be177f9a54672071626045fc8c3ec6b2
trak-dev <yann.picaud@gmail.com>
2023-11-08 13:53:50 +0100

    added reverse function~

--- index.js
+++ index.js
@@ -28,15 +28,53 @@ const IntToRoman = (num) => {
     return roman;
 }
 
+const RomanToInt = (roman) => {
+    let num = 0;
+    for (i in romanMap) {
+        while (roman.indexOf(i) === 0) {
+            num += romanMap[i];
+            roman = roman.replace(i, '');
+        }
+    }
+    return num;
+}
+
 app.get('/', (req, res) => {
-    const form = `
+    const form1 = `
         <form action="/roman" method="get">
             <label for="num">Enter a number:</label>
             <input type="number" id="num" name="num" required>
             <button type="submit">Convert to Roman</button>
         </form>
     `;
-    res.send(form);
+    const form2 = `
+        <form action="/int" method="get">
+            <label for="roman">Enter a Roman numeral:</label>
+            <input type="text" id="roman" name="roman" required>
+            <button type="submit">Convert to Integer</button>
+        </form>
+    `;
+    res.send(form1 + form2);
+});
+
+app.get('/roman', (req, res) => {
+    try {
+        const num = parseInt(req.query.num);
+        const roman = IntToRoman(num);
+        res.send(`The Roman numeral equivalent of ${num} is ${roman}`);
+    } catch (error) {
+        res.send("Invalid input");
+    }
+});
+
+app.get('/int', (req, res) => {
+    try {
+        const roman = req.query.roman.toUpperCase();
+        const num = RomanToInt(roman);
+        res.send(`The integer equivalent of ${roman} is ${num}`);
+    } catch (error) {
+        res.send("Invalid input");
+    }
 });
 
 app.get('/roman', (req, res) => {
