5554ba73ab9259c79ed929a6865e6241e76e8b00
Ilies T <47112446+ilies-t@users.noreply.github.com>
2023-11-09 19:46:47 +0100

    CVAR-24

--- tests/utils/number.util.spec.js
+++ tests/utils/number.util.spec.js
@@ -18,3 +18,18 @@ describe('Testing romain > arabic conversion', () => {
         expect(NumberUtil.romanToInt('CM').num).toBe(900);
     });
 });
+
+describe('Testing arabic > roman conversion', () => {
+    test('1 should return I', () => {
+        expect(NumberUtil.intToRoman(1).roman).toBe('I');
+    });
+    test('2 should return II', () => {
+        expect(NumberUtil.intToRoman(2).roman).toBe('II');
+    });
+    test('70 should return LXX', () => {
+        expect(NumberUtil.intToRoman(70).roman).toBe('LXX');
+    });
+    test('900 should return CM', () => {
+        expect(NumberUtil.intToRoman(900).roman).toBe('CM');
+    });
+});
